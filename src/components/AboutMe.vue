6375a0c0168e3e482157ab41a6880a886ef66dac
Robert Benjamin <rotberj@gmail.com>
2016-11-12 18:40:36 -0800

    Open about page links in a new tab

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -3,7 +3,7 @@
     <h1 class="header">What's this about?</h1>
     <img src="/static/img/about.jpg" class="image">
     <p class="details">This is a place for me to keep note of books I've read.</p>
-    <p>It was built using <a href="https://vuejs.org/">Vue.js</a> with book covers from <a href="https://www.amazon.com/books-used-books-textbooks/b/ref=topnav_storetab_b">Amazon</a>.</p>
+    <p>It was built using <a href="https://vuejs.org/" target="_blank">Vue.js</a> with book covers from <a href="https://www.amazon.com/books-used-books-textbooks/b/ref=topnav_storetab_b" target="_blank">Amazon</a>.</p>
   </section>
 </template>
 
