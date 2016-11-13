0688096ccf194cedb3d392624b8d6aeab15d7e08
Robert Benjamin <rotberj@gmail.com>
2016-11-12 18:54:45 -0800

    Add automatic deploy to Git hook

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer-bar">
     <p class="description">
-      <a href="http://robertbenjamin.co">•Made with ♥ in Seattle•</a>
+      <a href="https://robertbenjamin.co">•Made with ♥ in Seattle•</a>
     </p>
   </footer>
 </template>
